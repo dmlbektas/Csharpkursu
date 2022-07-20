cc2045c23707e4f3bab7cb32bcf251b5ffb8e1ab
damla <105720061+dmlbektas@users.noreply.github.com>
2022-07-21 00:19:47 +0300

    Add files via upload

---
+++ demo1.js
@@ -0,0 +1,15 @@
+var sayi1 = 290
+var sayi2 = 30
+var sayi3 = 233
+
+//en büyük sayıyı bulma
+
+var enBuyuk = sayi1 //29
+
+if(enBuyuk<sayi2){
+    enBuyuk = sayi2; //30
+}
+if(enBuyuk<sayi3){
+    enBuyuk =sayi2 //30
+}
+console.log ("bu sayıların en büyüğü = " +enBuyuk)
