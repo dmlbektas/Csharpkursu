d035e77ec006f97b1e89bbbf6643b4647654f218
damla <105720061+dmlbektas@users.noreply.github.com>
2022-07-22 18:12:59 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+
+namespace ConsoleApp1
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var number = 200;
+            if (number >=0 && number <= 100)
+            {
+                Console.WriteLine("number is between 0-100");
+            }
+            else if (number>100 && number<=200)
+            {
+                Console.WriteLine("number is between 101-200");
+            }
+            else if(number> 200 || number <0)
+            {
+                Console.WriteLine("number is less than 0 or greater than 200");
+            }
+
+            Console.ReadLine();
+            
+        }
+    }
+}
